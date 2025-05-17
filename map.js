b011ed65c35cc1ce6ce401edeb87c60ab1f1c8bb
Viki Shi <vishi@ucsd.edu>
2025-05-17 16:40:27 -0700

    g

--- map.js
+++ map.js
@@ -1,7 +1,8 @@
+// map.js
 import mapboxgl from 'https://cdn.jsdelivr.net/npm/mapbox-gl@2.15.0/+esm';
 import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm';
 
-mapboxgl.accessToken = 'pk.eyJ1IjoidmlraS1zaGkiLCJhIjoiY21hc203ODZqMGxyaTJzcHZlYTNldTZjdiJ9.Px2_WlK9ehu9DfQO-BaZCA';
+mapboxgl.accessToken = 'pk.eyJ1IjoidmlraS1zaGkiLCJhIjoiY21hc203ODZqMGxyaTJzcHZlYTNldTZjdiJ9.Px2_WlK9ehu9DfQO-BaZCA'; // Replace with your token
 
 const map = new mapboxgl.Map({
   container: 'map',
@@ -12,79 +13,41 @@ const map = new mapboxgl.Map({
   maxZoom: 18,
 });
 
-let stations = [];
-let circles;
-
-function getCoords(station) {
-  const point = new mapboxgl.LngLat(+station.Long, +station.Lat);
-  const { x, y } = map.project(point);
-  return { cx: x, cy: y };
-}
-
-function updatePositions() {
-  circles
-    .attr('cx', (d) => getCoords(d).cx)
-    .attr('cy', (d) => getCoords(d).cy);
-}
-
 map.on('load', async () => {
+  // Add Boston bike lanes
   map.addSource('boston_route', {
     type: 'geojson',
     data: 'https://bostonopendata-boston.opendata.arcgis.com/datasets/boston::existing-bike-network-2022.geojson',
   });
+
   map.addLayer({
     id: 'bike-lanes',
     type: 'line',
     source: 'boston_route',
     paint: {
-      'line-color': 'green',
+      'line-color': '#32D400',
       'line-width': 3,
-      'line-opacity': 0.4,
+      'line-opacity': 0.5,
     },
   });
 
+  // Add Cambridge bike lanes
   map.addSource('cambridge_route', {
     type: 'geojson',
     data: 'https://raw.githubusercontent.com/cambridgegis/cambridgegis_data/main/Recreation/Bike_Facilities/RECREATION_BikeFacilities.geojson',
   });
+
   map.addLayer({
     id: 'cambridge-bike-lanes',
     type: 'line',
     source: 'cambridge_route',
     paint: {
-      'line-color': 'green',
+      'line-color': '#1E90FF',
       'line-width': 3,
-      'line-opacity': 0.4,
+      'line-opacity': 0.5,
     },
   });
 
-  // Append SVG to canvas container
-  const container = map.getCanvasContainer();
-  const svg = d3.select(container).append('svg');
-
-  const jsonURL = 'https://dsc106.com/labs/lab07/data/bluebikes-stations.json';
-
-  try {
-    const jsonData = await d3.json(jsonURL);
-    stations = jsonData.data.stations;
-
-    circles = svg.selectAll('circle')
-      .data(stations)
-      .enter()
-      .append('circle')
-      .attr('r', 5)
-      .attr('fill', 'steelblue')
-      .attr('stroke', 'white')
-      .attr('stroke-width', 1)
-      .attr('opacity', 0.8);
-
-    updatePositions();
-
-    map.on('move', updatePositions);
-    map.on('zoom', updatePositions);
-    map.on('resize', updatePositions);
-    map.on('moveend', updatePositions);
-  } catch (err) {
-    console.error('Error loading station data:', err);
-  }
+  // Confirm everything loaded
+  console.log('Mapbox GL JS Loaded:', mapboxgl);
 });
