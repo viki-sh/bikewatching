f01539433d669b5003ef64b659596e91cdd58646
Viki Shi <vishi@ucsd.edu>
2025-05-17 15:37:53 -0700

    g

--- map.js
+++ map.js
@@ -1,12 +1,10 @@
+
 import mapboxgl from 'https://cdn.jsdelivr.net/npm/mapbox-gl@2.15.0/+esm';
 import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm';
-
 
 mapboxgl.accessToken = 'pk.eyJ1IjoidmlraS1zaGkiLCJhIjoiY21hc203ODZqMGxyaTJzcHZlYTNldTZjdiJ9.Px2_WlK9ehu9DfQO-BaZCA'; // replace with your actual token
 
 
-
-// Initialize the map
 const map = new mapboxgl.Map({
   container: 'map',
   style: 'mapbox://styles/mapbox/streets-v12',
@@ -16,9 +14,8 @@ const map = new mapboxgl.Map({
   maxZoom: 18
 });
 
-// Step 2.1 & 3.1 logic
 map.on('load', async () => {
-  // Boston bike lanes
+  // Step 2.1: Boston
   map.addSource('boston_route', {
     type: 'geojson',
     data: 'https://bostonopendata-boston.opendata.arcgis.com/datasets/boston::existing-bike-network-2022.geojson',
@@ -35,7 +32,7 @@ map.on('load', async () => {
     },
   });
 
-  // Cambridge bike lanes
+  // Step 2.3: Cambridge
   map.addSource('cambridge_route', {
     type: 'geojson',
     data: 'https://raw.githubusercontent.com/cambridgegis/cambridgegis_data/main/Recreation/Bike_Facilities/RECREATION_BikeFacilities.geojson',
@@ -52,7 +49,7 @@ map.on('load', async () => {
     },
   });
 
-  // Step 3.1: Fetch Bluebikes station data
+  // Step 3.1: Load Bluebike station JSON
   let jsonData;
   try {
     const jsonurl = 'https://dsc106.com/labs/lab07/data/bluebikes-stations.json';
