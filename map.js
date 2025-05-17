28eb1d0fd91dffe1518806097f54dbc52d8d8b0a
Viki Shi <vishi@ucsd.edu>
2025-05-17 13:21:31 -0700

    g

--- map.js
+++ map.js
@@ -21,10 +21,10 @@ map.on('load', async () => {
   };
 
   // Boston bike lanes
-  map.addSource('boston_route', {
+    map.addSource('boston_route', {
     type: 'geojson',
     data: 'https://bostonopendata-boston.opendata.arcgis.com/datasets/boston::existing-bike-network-2022.geojson',
-  });
+    });
 
   map.addLayer({
     id: 'bike-lanes-boston',
@@ -39,10 +39,14 @@ map.on('load', async () => {
     data: 'https://raw.githubusercontent.com/cambridgegis/cambridgegis_data/main/Recreation/Bike_Facilities/RECREATION_BikeFacilities.geojson',
   });
 
-  map.addLayer({
-    id: 'bike-lanes-cambridge',
-    type: 'line',
-    source: 'cambridge_route',
-    paint: lineStyle,
-  });
+map.addLayer({
+  id: 'bike-lanes',
+  type: 'line',
+  source: 'cambridge_route',
+  paint: {
+    'line-color': 'green',
+    'line-width': 3,
+    'line-opacity': 0.4,
+  },
+});
 });
